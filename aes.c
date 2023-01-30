501a7658f1976026713b5ecc1f3a59a2633c0d65
M. Ali Khan <45436022+mhdl1991@users.noreply.github.com>
2023-01-30 15:37:48 +0000

    Update aes.c

--- aes.c
+++ aes.c
@@ -6,10 +6,14 @@
 
 #include <stdint.h>
 
-#define ROWS 4 //columns of  AES  state
-#define WORDSKEY 32 //32-bit words in a key
-#define KEYLEN 16 //key length in bytes (128 bit)
-#define ROUNDS 10 //rounds of AES cipher
+//Use standard names and definitions given in official standards docment
+
+#define Nb 4 		//columns of  AES  state
+#define Nk 32 		//32-bit words in a key
+#define KEYLEN 16 	//key length in bytes (128 bit)
+#define Nr 10 		//rounds of AES cipher
+
+
 
 
 //TODO
