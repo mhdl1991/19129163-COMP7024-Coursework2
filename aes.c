73d055449f47de2e80bd7df386fde4629acff6fa
M. Ali Khan <45436022+mhdl1991@users.noreply.github.com>
2023-01-30 15:11:52 +0000

    Create aes.c

---
+++ aes.c
@@ -0,0 +1,15 @@
+/*
+	An implementation of AES encryption algorithm for a file encryption software
+	To be done in C
+	
+*/
+
+#include <stdint.h>
+
+#define ROWS 4 //columns of  AES  state
+#define WORDSKEY 32 //32-bit words in a key
+#define KEYLEN 16 //key length in bytes (128 bit)
+#define ROUNDS 10 //rounds of AES cipher
+
+
+//TODO
