fb37f036cfe0eb479956fe5ce69fc98027f68c21
mhdl1991 <mohammad.ali1991@live.com>
2023-02-06 13:16:37 +0000

    Update

--- aes.c
+++ aes.c
@@ -1,19 +1,31 @@
 /*
 	An implementation of AES encryption algorithm for a file encryption software
 	To be done in C
+
+	SubBytes
+	ShiftRow
+	MixColumn
+	AddRoundKey
 	
 */
-
 #include <stdint.h>
 
-//Use standard names and definitions given in official standards docment
 
-#define Nb 4 		//columns of  AES  state
-#define Nk 32 		//32-bit words in a key
-#define KEYLEN 16 	//key length in bytes (128 bit)
-#define Nr 10 		//rounds of AES cipher
+int encryptAll() { return 0; }
+
+int decryptAll() { return 0; }
 
 
+int main(int argc char* argv) {
+	
+	// entry point
+	
+	
+	
+	return 0;
+}
 
 
-//TODO
+
+
+
