086b942fd22f1bb2857feb83d4176b2a671262ba
mhdl1991 <mohammad.ali1991@live.com>
2023-02-12 14:00:16 +0000

    changing some names and plans

---
+++ main.c
@@ -0,0 +1,46 @@
+/*
+	An implementation of AES encryption algorithm for a file encryption software
+	To be done in C
+
+	SubBytes
+	ShiftRow
+	MixColumn
+	AddRoundKey
+	
+*/
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stddef.h>
+#include "aes.h"
+
+#define BUFFER_SIZE 255
+
+int encryptFile() {}
+
+int decryptFile() {}
+
+int encryptAll() { 
+	// encrypt all files
+	int success = 0;
+	return success; 
+}
+
+int decryptAll() {
+	int success = 0;
+	return success; 
+}
+
+int main(int argc char* argv) {
+	// entry point
+	// this needs to encrypt all the files in a hard disk and be able to decrypt them all.
+	FILE *fin *fout;
+	char buffer[BUFFER_SIZE];
+	
+	return 0;
+}
+
+
+
+
+
